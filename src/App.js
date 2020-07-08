7ce4964f5c3dfb83bdadfd1bd47d849ebab75d88
Najathi <najathi777@gmail.com>
2020-07-08 19:45:27 +0530

    install metirial ui messager cmp

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { FormControl, InputLabel, Input } from "@material-ui/core";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h2>Facebook Messenger Colon</h2>
+
+            <FormControl>
+                <InputLabel htmlFor="my-input">Email address</InputLabel>
+                <Input id="my-input" aria-describedby="my-helper-text" />
+            </FormControl>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Messanger/Messenger.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Messenger.css";
+
+const Messenger = () => {
+    return <div></div>;
+};
+
+export default Messenger;
